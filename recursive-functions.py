b6d274a9a8c929c8d4824254be10a0b9574d4da6
v1shay <v.agrwl17@gmail.com>
2026-01-17 15:18:07 -0800

    factorials, sums, fibbonaci

---
+++ recursive-functions.py
@@ -0,0 +1,33 @@
+# # a=int(input("Enter a number: "))
+# # product=1
+# # for c in range(a, 0, -1):
+# #     product=product*c 
+# #     print(product)
+
+# # def fibbonaci():
+# #     a=[0, 1]
+# #     n=int(input("Enter a number: "))
+# #     while len(a)<=n: 
+# #         c=a[-1] + a[-2]
+# #     print(c)
+# # fibbonaci()
+
+# # def factorial(n):
+# #     if n==1:
+# #         return 1
+# #     else: 
+# #         return n*factorial(n-1) 
+
+# # a=factorial(4)
+# # print(a)  
+
+# def sum(n):
+#     if n==1:
+#         return 1
+#     else:
+#         return (n%10+sum(n//10))
+    
+    
+# a=sum(1234)
+# print(a)
+
